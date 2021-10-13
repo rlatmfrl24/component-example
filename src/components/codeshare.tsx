0986a89c14c8cbca3f49024c8b771be38fd591ef
SulfurBottom <397love@gmail.com>
2021-10-13 16:23:22 +0900

    코드 쉐어 화면 3분할 적용

--- src/components/codeshare.tsx
+++ src/components/codeshare.tsx
@@ -2,9 +2,37 @@ import Editor, { Monaco } from "@monaco-editor/react";
 import { useEffect, useRef, useState } from "react";
 import { fromMonaco } from "@hackerrank/firepad";
 import firebase from "firebase";
+import { useRecoilValue } from "recoil";
+import { userNameState } from "../store/basic";
 
 export const CodeShare = () => {
+  const userName = useRecoilValue(userNameState);
+
+  const htmlEditorRef = useRef(null);
+  const cssEditorRef = useRef(null);
+  const jsEditorRef = useRef(null);
+
+  const [htmlEditorLoaded, setHtmlEditorLoaded] = useState(false);
+  const [cssEditorLoaded, setCssEditorLoaded] = useState(false);
+  const [jsEditorLoaded, setJsEditorLoaded] = useState(false);
+
+  function handleHtmlEditorDidMount(editor: any, monaco: Monaco) {
+    htmlEditorRef.current = editor;
+    setHtmlEditorLoaded(true);
+  }
+
+  function handleCssEditorDidMount(editor: any, monaco: Monaco) {
+    cssEditorRef.current = editor;
+    setCssEditorLoaded(true);
+  }
+
+  function handleJsEditorDidMount(editor: any, monaco: Monaco) {
+    jsEditorRef.current = editor;
+    setJsEditorLoaded(true);
+  }
+
   const editorRef = useRef(null);
+
   const [editorLoaded, setEditorLoaded] = useState(false);
 
   function handleEditorDidMount(editor: any, monaco: Monaco) {
@@ -13,29 +41,52 @@ export const CodeShare = () => {
   }
 
   useEffect(() => {
-    if (!editorLoaded) {
+    if (!htmlEditorLoaded || !cssEditorLoaded || !jsEditorLoaded) {
       // If editor is not loaded return
       return;
     }
+    const htmlCodeDBRef = firebase.database().ref().child("pair001");
+    const cssCodeDBRef = firebase.database().ref().child("pair002");
+    const jsCodeDBRef = firebase.database().ref().child("pair003");
 
-    const dbRef = firebase.database().ref().child("pair001"); // Can be anything in param, use unique string for unique code session
-    const firepad = fromMonaco(dbRef, editorRef.current!);
-    const name = prompt("Enter your Name :"); // Name to highlight who is editing where in the code
-    if (name) {
-      firepad.setUserName(name);
-    }
-  }, [editorLoaded]);
+    const htmlFirePad = fromMonaco(htmlCodeDBRef, htmlEditorRef.current!);
+    const cssFirePad = fromMonaco(cssCodeDBRef, cssEditorRef.current!);
+    const jsFirePad = fromMonaco(jsCodeDBRef, jsEditorRef.current!);
+
+    htmlFirePad.setUserName(userName);
+    cssFirePad.setUserName(userName);
+    jsFirePad.setUserName(userName);
+  }, [htmlEditorLoaded, cssEditorLoaded, jsEditorLoaded]);
 
   return (
     <div>
-      <Editor
-        height="90vh"
-        width="30vw"
-        defaultLanguage="javascript"
-        theme="vs-dark"
-        defaultValue="// Welcome to My Editor"
-        onMount={handleEditorDidMount}
-      />
+      <div style={{ width: "30vw", float: "left" }}>
+        <Editor
+          height="90vh"
+          defaultLanguage="html"
+          theme="vs-dark"
+          defaultValue="// Welcome to My Editor"
+          onMount={handleHtmlEditorDidMount}
+        />
+      </div>
+      <div style={{ width: "30vw", float: "left" }}>
+        <Editor
+          height="90vh"
+          defaultLanguage="css"
+          theme="vs-dark"
+          defaultValue="// Welcome to My Editor"
+          onMount={handleCssEditorDidMount}
+        />
+      </div>
+      <div style={{ width: "30vw", float: "left" }}>
+        <Editor
+          height="90vh"
+          defaultLanguage="javascript"
+          theme="vs-dark"
+          defaultValue="// Welcome to My Editor"
+          onMount={handleJsEditorDidMount}
+        />
+      </div>
     </div>
   );
 };
